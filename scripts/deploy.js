05cd50344132e2f84c71f46abc4f06599d9cff99
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-06-18 20:48:50 +0530

    tested on hardhat

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,23 +6,57 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+async function getBalances(address){
+  const balanceBigInt = await hre.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+async function consoleBalances(addresses){
+  let counter=0;
+
+  for(const address of addresses){
+    console.log(`Address ${counter} balance :`,await getBalances(address));
+    counter++;
+  }
+}
+
+async function consoleMemos(memos){
+  for(const memo of memos){
+    const timestamp=memo.timestamp; 
+    const  name= memo.name; 
+    const from=memo.from; 
+    const message=memo.message;
+    console.log(`At ${timestamp},name ${name}, address ${from},message ${message}`);
+  }
+}
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [owner,from1,from2,from3]=await hre.ethers.getSigners();
+  const chai = await hre.ethers.getContractFactory("Chai");
+  const contract =await chai.deploy(); //instace of contract 
+  
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  await contract.deployed();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  console.log("Address of contract : ",contract.address);
 
-  await lock.waitForDeployment();
+  const addresses=[owner.address,from1.address,from2.address,from3.address];
+  console.log("Before buying: ");
+  await consoleBalances(addresses);
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const amount = {value: hre.ethers.utils.parseEther("1")}
+
+  await contract.connect(from1).buyChai("from1","very nice chai 1",amount);
+  await contract.connect(from2).buyChai("from2","very nice chai 2",amount);
+  await contract.connect(from3).buyChai("from3","very nice chai 3",amount);
+
+  console.log("After buying: ");
+  await consoleBalances(addresses);
+
+  const memos = await contract.getMemos();
+  consoleMemos(memos);
+
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
