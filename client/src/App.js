03d49294803480acbc2232406bc9e9328317a8b2
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2023-06-19 16:28:22 +0530

    client side contract setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,35 @@
 import './App.css';
 import { ethers } from 'ethers';
 import abi from './contract/Chai.json'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [state,setState]=useState({
+    provider:null,
+    signer:null,
+    contract:null
+  })
+
+  useEffect(()=>{
+    const connectWallet = async()=>{
+      const contractAddress="0xB1c628c997c2455d5e44acBc8262b4376637d0B9";
+      const contractABI = abi.abi;
+      try {
+        const {ethereum}=window;
+        if(ethereum){
+          const account=await ethereum.request({method:"eth_requestAccounts"})
+        }
+        const provider= new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress,contractABI,signer);
+        setState({provider,signer,contract});
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    connectWallet();
+  },[])
+  console.log(state);
   return (
     <div className="App">
 
